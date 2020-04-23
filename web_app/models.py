f70656681378eceb38f73c28bb2250c99efb04fc
Pyrom E <e.pyrom@gmail.com>
2020-04-23 00:36:50 -0400

    __init__.py updated

--- web_app/models.py
+++ web_app/models.py
@@ -1,6 +1,6 @@
 # web_app/models.py
 
-from flask_sqlaclhemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
@@ -9,16 +9,16 @@ migrate = Migrate()
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.column(db.String(128))
+    title = db.Column(db.String(128))
     author_id = db.Column(db.String(128))
 
 def parse_records(database_records):
     """
-    A helper method for converting a list of database record objects into a list of dictionaries, so they can be returned as JSON.
+    A helper method for converting a list of database record objects into a list of dictionaries, so they can be returned as JSON
 
     Param: database_records (a list of db.Model instances)
 
-    Example: parse_records (User.query.all())
+    Example: parse_records(User.query.all())
 
     Returns: a list of dictionaries, each corresponding to a record, like...
         [
@@ -26,11 +26,11 @@ def parse_records(database_records):
             {"id": 2, "title": "Book 2"},
             {"id": 3, "title": "Book 3"},
         ]
-        """
-        parsed_records = []
-        for record in database_records:
-            print(record)
-            parsed_record = record.__dict__
-            del parsed_record["_sa_instance_state"]
-            parsed_records.append(parsed_record)
-        return parsed_records
+    """
+    parsed_records = []
+    for record in database_records:
+        print(record)
+        parsed_record = record.__dict__
+        del parsed_record["_sa_instance_state"]
+        parsed_records.append(parsed_record)
+    return parsed_records
