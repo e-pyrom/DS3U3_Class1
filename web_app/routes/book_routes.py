c1958d118e0a2075045df40249377082ca8a1617
Pyrom E <e.pyrom@gmail.com>
2020-04-22 22:57:10 -0400

    Render books JSON

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/book_routes.py
 
-from flask import Blueprint, jsonify, request, render_template #, flash, redirect
+from flask import Blueprint, jsonify#, request, render_template #, flash, redirect
 
 book_routes = Blueprint("book_routes", __name__)
 
@@ -13,25 +13,25 @@ def list_books():
     ]
     return jsonify(books)
 
-@book_routes.route("/books")
-def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
-    return render_template("books.html", message="Here's some books", books=books)
+# @book_routes.route("/books")
+# def list_books_for_humans():
+#     books = [
+#         {"id": 1, "title": "Book 1"},
+#         {"id": 2, "title": "Book 2"},
+#         {"id": 3, "title": "Book 3"},
+#     ]
+#     return render_template("books.html", message="Here's some books", books=books)
 
-@book_routes.route("/books/new")
-def new_book():
-        return render_template("new_book.html")
+# @book_routes.route("/books/new")
+# def new_book():
+#         return render_template("new_book.html")
 
-@book_routes.route("/books/create", methods=['POST'])
-def create_book():
-    print("FORM DATA:", dict(request.form))
-    # todo: store in database
-    return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
-    })
-    #flash(f"Book '{new_book.title}' created successfully!", "success")
-    #return redirect(f"/books")    
+# @book_routes.route("/books/create", methods=['POST'])
+# def create_book():
+#     print("FORM DATA:", dict(request.form))
+#     # todo: store in database
+#     return jsonify({
+#         "message": "BOOK CREATED OK (TODO)",
+#     })
+#     #flash(f"Book '{new_book.title}' created successfully!", "success")
+#     #return redirect(f"/books")    
